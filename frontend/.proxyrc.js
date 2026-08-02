0914eb9cc470f6804805637c2440912f9b83be12
Sophie Quinn-Graham <7331+thissophie@users.noreply.github.com>
2026-08-02 13:36:12 +1000

    fix: frontend - dev server proxy api changed

--- frontend/.proxyrc.js
+++ frontend/.proxyrc.js
@@ -5,16 +5,19 @@ const cookie = 'NDV_AUD=' + encodeURIComponent(process.env.NDV_AUD_COOKIE);
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware('/api', {
+    createProxyMiddleware({
+      pathFilter: '/api',
       target: 'https://live.aws.nextdayvideo.com.au:443/',
       agent: globalAgent,
       cookieDomainRewrite: 'localhost',
       headers: {
         host: 'live.aws.nextdayvideo.com.au',
       },
-      onProxyReq: function (proxyReq) {
-        console.log('setting cookie');
-        proxyReq.setHeader('cookie', cookie);
+      on: {
+        proxyReq: function (proxyReq) {
+          console.log('setting cookie');
+          proxyReq.setHeader('cookie', cookie);
+        },
       },
     }),
   );
